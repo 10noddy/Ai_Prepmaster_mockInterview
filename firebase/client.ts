9d0a8c01c010b33c85a41f856429e58c9e539775
mayank repo <22052560@kiit.ac.in>
2025-08-12 02:06:00 +0530

    Firebase setup

---
+++ firebase/client.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAt0fnbvKeHQ65fDNXJRFce67W_7M5I5v0",
+  authDomain: "prepwise-17042004.firebaseapp.com",
+  projectId: "prepwise-17042004",
+  storageBucket: "prepwise-17042004.firebasestorage.app",
+  messagingSenderId: "398887587162",
+  appId: "1:398887587162:web:50f1d369b144ed019ef763",
+  measurementId: "G-5H6RKQHG1L"
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig): getApp();
+
+export const auth=getAuth(app);
