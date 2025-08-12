a8ddee88db61a88aa5d30b6a3a039ba12f424e11
mayank repo <22052560@kiit.ac.in>
2025-08-13 03:38:08 +0530

    implement interview generation page ui

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Agent from '@/components/Agent'
+
+
+const page = () => {
+  return (
+    <>
+    <h3> interview generation</h3>
+
+    <Agent  />
+    </>
+  )
+}
+
+export default page
